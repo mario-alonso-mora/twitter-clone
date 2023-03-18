16847f43fedfdcbd3c3c913055e6550cbaf4dd75
Mario <marioalonsomora84@gmail.com>
2023-03-18 07:21:41 +0100

    Twitter-clone completed

---
+++ hooks/useCurrentUser.ts
@@ -0,0 +1,18 @@
+
+
+import fetcher from '@/libs/fetcher';
+import useSWR from 'swr';
+
+
+const useCurrentUser = () => {
+  const { data, error, isLoading, mutate } = useSWR('/api/current', fetcher);
+
+  return {
+    data,
+    error,
+    isLoading,
+    mutate
+  }
+};
+
+export default useCurrentUser;
